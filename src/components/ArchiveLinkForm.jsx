3f00f3ba60b76f37abbcae2fe74dd3931386aebd
CAndRyan <CAndRyan@users.noreply.github.com>
2022-04-23 17:35:19 -0400

    Style app

--- src/components/ArchiveLinkForm.jsx
+++ src/components/ArchiveLinkForm.jsx
@@ -1,30 +1,50 @@
 ﻿import styles from './styles/ArchiveLinkForm';
 
-const saveLinkData = () => {
-	//
+const saveData = async () => {
+	// TODO: connect to firestorm
 };
 
-const onFormSubmit = (event) => {
-	debugger;
+const onFormSubmit = async (event) => {
+	const { target } = event;
+	const formData = new FormData(target);
+
+	for (var [key, value] of formData.entries())
+		console.log(key, value);
+
+	try {
+		await saveData();
+		target.reset();
+		// TODO: display success message
+	} catch (error) {
+		console.error(error);
+		// TODO: display error message
+	}
+
+	event.preventDefault();
+	return false;
 };
 
 const ArchiveLinkForm = () => {
 	
 	return (
-		<form className={styles.linkForm} onSubmit={onFormSubmit}>
+		<form className={styles.linkForm} onSubmit={onFormSubmit} autocomplete="off">
 			<table>
 				<tbody>
 					<tr>
 						<td>URL</td>
-						<td><input type="text" name="url" /></td>
+						<td className={styles.inputColumn}><input type="url" name="url" required placeholder="e.g. https://mtbjorn.net" /></td>
+					</tr>
+					<tr>
+						<td>Title</td>
+						<td className={styles.inputColumn}><input type="text" name="title" required /></td>
 					</tr>
 					<tr>
 						<td><label title="A comma-separated list">Labels</label></td>
-						<td><input type="text" name="labels" /></td>
+						<td className={styles.inputColumn}><input type="text" name="labels" required placeholder="e.g. programming, politics" /></td>
 					</tr>
 					<tr>
-						<td>Description</td>
-						<td><input type="text" name="description" /></td>
+						<td><label title="https://commonmark.org/">Description</label></td>
+						<td className={styles.inputColumn}><textarea name="description" placeholder="HINT: try CommonMark markdown" /></td>
 					</tr>
 				</tbody>
 			</table>
